8c257f543f10c88db283e70b094828538519bca4
Abhijeet Ugale <ugaleabhijeet03@gmail.com>
2026-01-03 12:52:35 +0530

    update file

--- client/src/components/ContactForm.jsx
+++ client/src/components/ContactForm.jsx
@@ -155,6 +155,8 @@ function ContactForm({ onSubmit }) {
             onChange={handleChange}
             onBlur={handleBlur}
             type="number"
+            minLength={10}
+            maxLength={10}
             className={`mt-1 w-full rounded-md border px-3 py-2 text-sm outline-none focus:border-slate-400 ${
               errors.phone && touched.phone
                 ? 'border-rose-300'
